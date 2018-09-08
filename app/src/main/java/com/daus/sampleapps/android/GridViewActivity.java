d165317cb96bc5181a855cc9c8f2e9cc05f02724
Ujuizi Lab's <daus.kurniawan@cheetah.ujuizi.com>
2018-09-08 20:45:23 +0700

    - Move GridItem inside GridViewActivity class - Move GridMenuAdapter inside GridViewActivity

--- app/src/main/java/com/daus/sampleapps/android/GridViewActivity.java
+++ app/src/main/java/com/daus/sampleapps/android/GridViewActivity.java
@@ -1,20 +1,27 @@
 package com.daus.sampleapps.android;
 
+import android.content.Context;
+import android.content.Intent;
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.GridLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.util.Log;
+import android.view.LayoutInflater;
 import android.view.View;
-import android.widget.Toast;
-
-import com.daus.sampleapps.android.adapters.gridview.GridViewItem;
-import com.daus.sampleapps.android.adapters.gridview.GridViewMenuAdapter;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import java.util.ArrayList;
 
 public class GridViewActivity extends AppCompatActivity {
 
     private GridViewActivity gridViewActivity;
+
+    ArrayList<GridViewItem> listMenu = new ArrayList<>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -33,10 +40,9 @@ public class GridViewActivity extends AppCompatActivity {
 
         gridView.setLayoutManager(new GridLayoutManager(this, numberOfColumns));// for grid layout
 
-        ArrayList<GridViewItem> listMenu = new ArrayList<>();
 
         GridViewItem itemSampleView = new GridViewItem();
-        itemSampleView.setName("Sample View");
+        itemSampleView.setName("View");
         itemSampleView.setDrawableId(getResources().getDrawable(R.drawable.sample_view_icon));
         listMenu.add(itemSampleView);
 
@@ -64,12 +70,116 @@ public class GridViewActivity extends AppCompatActivity {
         GridViewMenuAdapter gridmenuAdapter = new GridViewMenuAdapter(gridViewActivity, listMenu);
         gridView.setAdapter(gridmenuAdapter);
 
-        gridmenuAdapter.setClickListener(new GridViewMenuAdapter.GridItemClickListener() {
+        gridmenuAdapter.setClickListener(new GridItemClickListener() {
             @Override
             public void onItemClick(View view, int position) {
-                Toast.makeText(gridViewActivity, "" + position,
-                        Toast.LENGTH_SHORT).show();
+                switch (position) {
+                    case 0:
+                        startActivity(new Intent(gridViewActivity, SampleViewListActivity.class));
+                        break;
+                }
             }
         });
+    }
+
+    public static class GridViewItem {
+
+        private String name;
+        private Drawable drawableId;
+
+
+        public Drawable getDrawableId() {
+            return drawableId;
+        }
+
+        public void setDrawableId(Drawable drawableId) {
+            this.drawableId = drawableId;
+        }
+
+        public String getName() {
+            return name;
+        }
+
+        public void setName(String name) {
+            this.name = name;
+        }
+    }
+
+    public static class GridViewMenuAdapter extends RecyclerView.Adapter<GridViewMenuAdapter.ViewHolder> {
+
+        private static final String TAG = "GridViewMenuAdapter";
+
+        private final ArrayList<GridViewItem> listMenu;
+        private final LayoutInflater inflater;
+        private final Context context;
+        private GridItemClickListener mClickListener;
+
+        public GridViewMenuAdapter(Context context, ArrayList<GridViewItem> listMenu) {
+            this.listMenu = listMenu;
+            this.context = context;
+            inflater = (LayoutInflater) context
+                    .getSystemService(LAYOUT_INFLATER_SERVICE);
+        }
+
+        @NonNull
+        @Override
+        public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+            View view = inflater.inflate(R.layout.grid_menu_layout, parent, false);
+            return new ViewHolder(view);
+        }
+
+        @Override
+        public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
+            Log.e(TAG, "onBindViewHolder position : " + position);
+            Log.e(TAG, "onBindViewHolder name : " + listMenu.get(position).getName());
+            try {
+                holder.textView.setText(listMenu.get(position).getName());
+                holder.imgView.setImageDrawable(listMenu.get(position).getDrawableId());
+            } catch (Exception e) {
+                Log.e(TAG, "onBindViewHolder : " + e.toString());
+                e.printStackTrace();
+            }
+        }
+
+        @Override
+        public long getItemId(int i) {
+            return i;
+        }
+
+        @Override
+        public int getItemCount() {
+            return listMenu.size();
+        }
+
+        // stores and recycles views as they are scrolled off screen
+        public class ViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener {
+            TextView textView;
+            ImageView imgView;
+
+            ViewHolder(View itemView) {
+                super(itemView);
+                textView = itemView.findViewById(R.id.tvGridMenuItem);
+                imgView = itemView.findViewById(R.id.imgGridMenuItem);
+                itemView.setOnClickListener(this);
+            }
+
+            @Override
+            public void onClick(View view) {
+                if (mClickListener != null) mClickListener.onItemClick(view, getAdapterPosition());
+            }
+        }
+
+        // allows clicks events to be caught
+        public void setClickListener(GridItemClickListener itemClickListener) {
+            this.mClickListener = itemClickListener;
+        }
+
+
+
+    }
+
+    // parent activity will implement this method to respond to click events
+    public interface GridItemClickListener {
+        void onItemClick(View view, int position);
     }
 }
